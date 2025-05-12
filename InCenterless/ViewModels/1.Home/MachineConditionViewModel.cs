abeafc81876af629af696d5951b6a17168f25834
Kim Dosung <dosung1678@naver.com>
2025-05-12 11:35:36 +0900

    Add project files.

---
+++ InCenterless/ViewModels/1.Home/MachineConditionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InCenterless.ViewModels._1.Home
+{
+    internal class MachineConditionViewModel
+    {
+    }
+}
